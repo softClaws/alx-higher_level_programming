c1cfd5dd8c9f45af1a564d48a9381209b0ba7a6f
ABDULKADIR <uabdulkadir06@gmail.com>
2022-10-26 19:34:32 +0000

    naming correction

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """
-Module 12-student
+Module 11-student
 
 Contains class Student
 that initializes public instance attributes first_name, last_name, and age,
-and has public method to_json that returns dictionary representation
-of requested attributes or all if none were requested
+and has public method to_json that retrieves its dictionary representation
 """
 
 
@@ -27,21 +26,10 @@ class Student():
         self.last_name = last_name
         self.age = age
 
-    def to_json(self, attrs=None):
+    def to_json(self):
         """
         Returns dictionary description with simple data structure
         (list, dictionary, dictionary, string)
         for JSON serialization of an object
-
-        Return:
-            Only return dict of attrs given to us
-            Return entire dict if no attrs given
         """
-        if attrs is None:
-            return self.__dict__
-        else:
-            dic = {}
-            for att in attrs:
-                if att in self.__dict__.keys():
-                    dic[att] = self.__dict__[att]
-            return dic
+        return self.__dict__
